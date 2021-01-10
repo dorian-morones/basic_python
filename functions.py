8d0aaa932d44f77b2b46f1b5d1f65015d90d75a3
Dorian Morones <dorianm393@gmail.com>
2021-01-10 10:23:41 -0800

    funciones

---
+++ functions.py
@@ -0,0 +1,14 @@
+def imprimir_mensaje(option):
+    print('mi mensaje')
+    print(option)
+
+opcion = int(input('Elige una opcion (1,2,3): '))
+
+if opcion == 1:
+    imprimir_mensaje(opcion)
+elif opcion == 2:
+  imprimir_mensaje(opcion)
+elif opcion == 3:
+  imprimir_mensaje(opcion)
+else:
+  print('seleccion no valida')
